4261cab3f5f48683b32f7161aed5a0b71a33726b
maNNIakk <renato.yancovit4@gmail.com>
2022-11-07 22:10:12 -0300

    Mochawesome config and 3 HTML reports

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,15 @@ const { defineConfig } = require("cypress");
 
 module.exports = 
 defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/report/mochawesome-report",
+    overwrite: true,
+    html: true,
+    json: false,
+    timestamp: "mmddyyyy_HHMMss"
+  },
+  recordKey: process.env.CYPRESS_RECORD_KEY,
   projectId: "fzfg3u",
   e2e: {
 
