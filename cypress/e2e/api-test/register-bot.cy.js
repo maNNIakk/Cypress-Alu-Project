2a9c3d5c51740809875d906de4aa757d0f467bd8
maNNIakkus <renato.yancovit4@gmail.com>
2022-11-12 10:14:29 -0300

    +Cycommands + Documentation + fillFormBot refactored

--- cypress/e2e/api-test/register-bot.cy.js
+++ cypress/e2e/api-test/register-bot.cy.js
@@ -10,23 +10,7 @@ describe('Cria Lead', () => {
 
     it.only('Cria Novo Formulário Loja', () =>{
 
-        cy.get('input[type="number"]').eq(0).type('167132');
-        cy.get('input[type="number"]').eq(1).type('50');
-        cy.get('input[readonly="readonly"]').eq(0).click();
-        cy.get('[class="b24-form-control-list-selector-item-title"').contains('span','RJ').click();
-        cy.get('input[name="phone"]').type('21964851522');
-        //cy.get('[style=""] > :nth-child(1) > :nth-child(1) > .b24-form-control-container > .b24-form-control').click();
-        cy.get('input[readonly="readonly"]').eq(23).click();
-        cy.contains('span','Rio de Janeiro - Barra da Tijuca').click();
-        cy.get('input[name="name"]').type('TESTE');
-        cy.get('input[name="email"]').type('teste@teste.com.br');
-        cy.get('input[readonly="readonly"').eq(28).click();
-        cy.get('.b24-form-control-list-selector-item-title > span').eq(2).click();
-        cy.get('input[readonly="readonly"').eq(29).click();
-        cy.get('.b24-form-control-list-selector-item-title > span').eq(2).click();
-        cy.get('input[value="82403"').click();
-        //Se descomentar abaixo envia o formulario para o ambiente de produção - NÃO DESCOMENTAR
-        //cy.get('form').submit();
+        cy.fillForm(Cypress.env('renato'),'xablau','21992535332','xablau@xablau.com',Cypress.env('ev1'));
 
     })
 
--- cypress/support/shortcuts.js
+++ cypress/support/shortcuts.js
@@ -13,3 +13,36 @@ Cypress.Commands.add('register',(email,fullName,userName,password) => {
     cy.get('input[formcontrolname="userName"]').type(userName);
     cy.get('input[formcontrolname="password"]').type(password);
 })
+
+Cypress.Commands.add('fillForm', (vendorId,clientName,clientPhone,clientEmail,bikeModel) => {
+
+    //Get and fill vendor id number
+    cy.get('input[type="number"]').eq(0).type(vendorId);
+    //Open State Dropdown menu
+    cy.get('input[readonly="readonly"]').eq(0).click();
+    //Select Rj in Dropdown menu
+    cy.get('[class="b24-form-control-list-selector-item-title"').contains('span','RJ').click();
+    //Get and fill phone field
+    cy.get('input[name="phone"]').type(clientPhone);
+    //Open City Dropdown menu
+    cy.get('input[readonly="readonly"]').eq(23).click();
+    //Select the city/area in City Dropdown menu
+    cy.contains('span','Rio de Janeiro - Barra da Tijuca').click();
+    //Get and fill client name
+    cy.get('input[name="name"]').type(clientName);
+    //Get and fill client email
+    cy.get('input[name="email"]').type(clientEmail);
+    //Open 
+    cy.get('input[readonly="readonly"').eq(28).click();
+    cy.get('.b24-form-control-list-selector-item-title > span').eq(2).click();
+    cy.get('input[readonly="readonly"').eq(29).click();
+    //bikeModel 1 = EV1 Sport / bikeModel 2 = EVS
+    cy.get('.b24-form-control-list-selector-item-title > span').eq(bikeModel).click();
+    cy.get('input[type="number"]').eq(1).type('50');
+    cy.get('input[value="82403"').click();
+    //Se descomentar abaixo envia o formulario para o ambiente de produção - NÃO DESCOMENTAR
+    //cy.get('form').submit();
+
+
+
+})
